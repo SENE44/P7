9de04bf0ecf1961523e37ef4d7f411bc88f60f05
SENE44 <132469539+SENE44@users.noreply.github.com>
2023-06-29 09:30:35 +0200

    end

---
+++ app.js
@@ -0,0 +1,48 @@
+const express=require('express')
+const mysql=require('mysql')
+const fs=require('fs')
+const bodyParser = require('body-parser')
+
+const app=express()
+app.use(bodyParser.urlencoded ({extended:true}))
+const connection=mysql.createConnection({
+    hostost:'localhost',
+    user:'root',
+    port:3306,
+    password:'sene',
+    database:'article'
+
+})
+connection.connect((err)=>{
+    if(err)throw err;
+    else{
+        console.log('connection ressie')
+
+    }
+})
+const reqgget=app.get(('/'), (req,res)=>{
+    connection.query('SELECT * FROM elements',(err,result)=>{
+        if(err)throw err;
+        res.send(result)
+    })
+}) 
+const reqpost=app.post(('/'),(req,res)=>{
+
+    connection.query(`INSERT INTO elements(id,titre,resumé,auteur,contenu,date_creation,date_m_a_j) 
+    VALUES 
+    ('4' ,'hdhggd','edede','rtttyr','dhdggdg','2002/02/02','2002/02/02')  `,(err,result)=>{
+        if(err) throw err;
+        res.send('erregister')
+    })
+})
+
+
+
+app.listen(3000,()=>{console.log('okok')})
+
+
+
+
+
+
+
